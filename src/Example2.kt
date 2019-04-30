7745a6496b40a07f8c3e46207b9011082f716807
Florian Herborn <florianherborn2293@t-online.de>
2019-04-30 14:02:00 +0200

    e1 + e2

---
+++ src/Example2.kt
@@ -0,0 +1,15 @@
+/**
+ * @author Florian Herborn
+ * @see 'MARTIN, Robert C. Clean code: a handbook of agile software craftsmanship. Pearson Education, 2009. S.18'
+ **/
+
+val theList: ArrayList<Array<Int>> = ArrayList()
+
+fun getThem(): ArrayList<Array<Int>> {
+    val list1 = ArrayList<Array<Int>>()
+    for (x in theList) {
+        if (x[0] == 4)
+            list1.add(x)
+    }
+    return list1
+}
