dd16bf04da64d11d1248fca221df71477213aa90
Florian Herborn <florianherborn2293@t-online.de>
2019-05-06 08:21:10 +0200

    Dirty 6

---
+++ src/Example6.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+/**
+ * Make Meaningful Distinctions
+ * @author Florian Herborn
+ * @see 'MARTIN, Robert C. Clean code: a handbook of agile software craftsmanship. Pearson Education, 2009. S.22'
+ **/
+
+class DtaRcrd102 {
+    private val genymdhms: Date = TODO()
+    private val modymdhms: Date = TODO()
+    private val pszqint: String = "102"
+}
