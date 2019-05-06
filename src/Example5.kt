6de31e8ac51f12a323c60243041e238a988f80e0
Florian Herborn <florianherborn2293@t-online.de>
2019-05-06 08:21:01 +0200

    Dirty 5

---
+++ src/Example5.kt
@@ -0,0 +1,9 @@
+/**
+ * Make Meaningful Distinctions
+ * @author Florian Herborn
+ * @see 'MARTIN, Robert C. Clean code: a handbook of agile software craftsmanship. Pearson Education, 2009. S.21'
+ **/
+
+fun getActiveAccount(): Any = TODO()
+fun getActiceAccounts(): Any = TODO()
+fun getAccountInfo(): Any = TODO()
