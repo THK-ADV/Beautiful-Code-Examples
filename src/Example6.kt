faf55b938ae335173ba08901d639cfa85e24f233
Florian Herborn <florianherborn2293@t-online.de>
2019-05-06 08:59:36 +0200

    Clean 6

--- src/Example6.kt
+++ src/Example6.kt
@@ -6,8 +6,8 @@ import java.util.*
  * @see 'MARTIN, Robert C. Clean code: a handbook of agile software craftsmanship. Pearson Education, 2009. S.22'
  **/
 
-class DtaRcrd102 {
-    private val genymdhms: Date = TODO()
-    private val modymdhms: Date = TODO()
-    private val pszqint: String = "102"
+class Customer {
+    private val generationTimestamp: Date = TODO()
+    private val modificationTimestamp: Date = TODO()
+    private val recordId: String = "102"
 }
