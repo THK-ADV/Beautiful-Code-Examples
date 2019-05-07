ea55a5c0c34490a0c68b0ba65e4865c03db3c13e
Florian Herborn <florianherborn2293@t-online.de>
2019-05-07 08:30:15 +0200

    Added 11

---
+++ src/example11/Example11.kt
@@ -0,0 +1,15 @@
+package example11
+
+/**
+ * Class Names
+ * @author Florian Herborn
+ * @see 'MARTIN, Robert C. Clean code: example3.getA handbook of agile software craftsmanship. Pearson Education, 2009. S.24'
+ **/
+object Example11 {
+
+    class UserManager
+    class UserProcessor
+    class UserData
+    class UserInfo
+
+}
---
+++ src/example11/Solution.kt
@@ -0,0 +1,15 @@
+package example11
+
+/**
+ * Class Names
+ * @author Florian Herborn
+ * @see 'MARTIN, Robert C. Clean code: example3.getA handbook of agile software craftsmanship. Pearson Education, 2009. S.24'
+ **/
+object Solution {
+
+    class UserDatabase
+    class UserUpdater
+    class User
+    class Address
+
+}
