c40eaf8c6427aba69af5fb160c83bdbd2b27b868
Florian Herborn <florianherborn2293@t-online.de>
2019-05-07 08:25:08 +0200

    Added 10

---
+++ src/example10/Solution.kt
@@ -0,0 +1,16 @@
+package example10
+
+/**
+ * Interfaces and Implementations
+ * @author Florian Herborn
+ * @see 'MARTIN, Robert C. Clean code: example3.getA handbook of agile software craftsmanship. Pearson Education, 2009. S.24'
+ **/
+object Solution {
+
+    interface Factory
+
+    class FactoryImpl: Factory
+
+    class CShapeFactory: Factory
+
+}
