8bcd549364be0277f9808ca0119a988b9d12fa20
Florian Herborn <florianherborn2293@t-online.de>
2019-05-07 12:25:05 +0200

    Added 14

---
+++ src/example14/Example14.kt
@@ -0,0 +1,13 @@
+package example14
+
+/**
+ * Function Names
+ * @author Florian Herborn
+ * @see 'MARTIN, Robert C. Clean code: example3.getA handbook of agile software craftsmanship. Pearson Education, 2009. S.26'
+ **/
+object Example14 {
+
+    fun holyHandGranade(): Unit = TODO()
+    fun whack(): Unit = TODO()
+    fun eatMyShorts(): Unit = TODO()
+}
---
+++ src/example14/Solution.kt
@@ -0,0 +1,14 @@
+package example14
+
+/**
+ * Function Names
+ * @author Florian Herborn
+ * @see 'MARTIN, Robert C. Clean code: example3.getA handbook of agile software craftsmanship. Pearson Education, 2009. S.25'
+ **/
+object Solution {
+
+    fun deleteItems(): Unit = TODO()
+    fun kill(): Unit = TODO()
+    fun abort(): Unit = TODO()
+
+}
